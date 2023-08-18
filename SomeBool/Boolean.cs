21258ee2d7812cf9fbd2ca617e9e52d94b2e8966
pogremix <pogremix@yandex.ru>
2023-08-18 21:23:33 +0300

    initial

---
+++ SomeBool/Boolean.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics.Metrics;
+
+namespace SomeBool
+{
+    public class Boolean
+    {
+        public static bool operator ==(Boolean boolean, bool value)
+        {
+            return value == true || true;
+        }
+
+        public static bool operator !=(Boolean boolean, bool value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SomeBool/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SomeBool
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var someBool = new Boolean();
+            bool result = someBool == true && someBool == false;
+            Console.WriteLine(result);
+        }
+    }
+}
